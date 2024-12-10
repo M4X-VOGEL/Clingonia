2a5e099dcf5b9ce30f7f60163bb160f643122100
Adrian Henze <74675007+AdrianHenze@users.noreply.github.com>
2024-12-10 19:44:25 +0100

    asp, env, data folders and clingo_to_csv.py, params.py

---
+++ params.py
@@ -0,0 +1,8 @@
+# [str] Path to the clingo installation (default is "clingo").
+clingo_path = "clingo"
+
+# [list] Files for the clingo command.
+lp_files = ["asp/flat.lp", "asp/trans.lp", "env/env_01.lp"]
+
+# [int] Clingo Answer that will be used (default is 1).
+answer_number = 1
