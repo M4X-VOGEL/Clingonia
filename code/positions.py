8287410d207934f7450ad8fbd2817e4b50a21461
Adrian Henze <74675007+AdrianHenze@users.noreply.github.com>
2025-02-11 09:04:58 +0100

    Strict positioning

--- code/positions.py
+++ code/positions.py
@@ -2,7 +2,8 @@ import pandas as pd
 import platform
 import subprocess
 from code.clingo_actions import clingo_to_df
-pd.set_option('display.max_rows', None)  # Anzeige aller DF-Zeilen
+
+invalid_path = None
 
 def position_df(tracks, trains, clingo_path, lp_files, answer_number):
     """
@@ -33,27 +34,28 @@ def build_df_pos(df_actions, trains, tracks):
     train_ids = {}
     # Calculate positions based on actions
     for _, row in df_actions.iterrows():
-        train_id = row['trainID']
+        id = row['trainID']
         action = row["action"]
         t = row["timestep"]
         # Set start position
-        if train_id not in train_ids:
-            x, y, dir = get_start_pos(train_id, trains)
+        if id not in train_ids:
+            x, y, dir = get_start_pos(id, trains)
             # Set start at timestep t-1
-            df_pos.loc[len(df_pos)] = [train_id, x, y, dir, t-1]
-            train_ids[train_id] = (x, y, dir)
+            df_pos.loc[len(df_pos)] = [id, x, y, dir, t-1]
+            train_ids[id] = (x, y, dir)
         else:
             # Use last position for next positions
-            x, y, dir = train_ids[train_id]
+            x, y, dir = train_ids[id]
         # Calculate next positions
-        x_new, y_new, dir_new = next_pos(x, y, action, dir, tracks)
-        df_pos.loc[len(df_pos)] = [train_id, x_new, y_new, dir_new, t]
-        train_ids[train_id] = (x_new, y_new, dir_new)
+        x_new, y_new, dir_new = next_pos(id, x, y, action, dir, tracks)
+        df_pos.loc[len(df_pos)] = [id, x_new, y_new, dir_new, t]
+        train_ids[id] = (x_new, y_new, dir_new)
     return df_pos
 
 def adjust_actions(df_pos, trains, df_actions, tracks):
     """Adjusts the last position with the station position.
     """
+    global invalid_path
     changed = True
     while changed:
         changed = False
@@ -73,7 +75,8 @@ def adjust_actions(df_pos, trains, df_actions, tracks):
             
             # Remove unnecessary spawn actions or
             # remove every action, if actions do not reach the target
-            if not (x_last == x_end and y_last == y_end):
+            if not (x_last == x_end and y_last == y_end) or invalid_path == train:
+                invalid_path = None
                 idxs = df_actions[df_actions['trainID'] == train].index
                 idx_to_drop = idxs[0]
                 df_actions.drop(index=idx_to_drop, inplace=True)
@@ -98,8 +101,6 @@ def ensure_train_spawns(df_pos, trains):
         x_val = train_row["x"]
         y_val = train_row["y"]
         dir_val = train_row["dir"]
-        e_dep = train_row["e_dep"]
-        l_arr = train_row["l_arr"]
         new_row = {
             "trainID": train_id,
             "x": x_val,
@@ -140,69 +141,116 @@ def get_start_pos(train_id, trains):
     return x, y, dir
 
 
-def next_pos(x, y, action, dir, tracks):
+def next_pos(id, x, y, action, dir, tracks):
     # Direction change
     if action in ["move_forward", "move_left", "move_right"]:
-        dir_new = dir_change(x, y, action, dir, tracks)
+        dir_new = dir_change(id, x, y, action, dir, tracks)
         x_new, y_new = pos_change(x, y, dir_new)
+        return x_new, y_new, dir_new
     else:  # wait-action
         return x, y, dir
-    return x_new, y_new, dir_new
 
 
-def dir_change(x, y, action, dir, tracks):
-    # Check, if x and y are valid coordinates
-    if y < 0 or y >= len(tracks) or x < 0 or x >= len(tracks[0]):
-        # Just keep moving forward. df_actions spawn moves reduction incoming.
+def dir_change(id, x, y, action, dir, tracks):
+    global invalid_path
+    # Check, if coordinates are valid
+    if not (0 <= y < len(tracks) and 0 <= x < len(tracks[0])):
+        # Invalid: No dir change: path will be adjusted later
+        if invalid_path == None:
+            invalid_path = id
         return dir
-    # Tracks with directional change
-    dir_tr = [4608, 16386, 72, 2064,
-              20994, 16458, 2136, 6672,
-              37408, 17411, 32872, 3089,
-              49186, 1097, 34864, 5633]
-    
+
+    track = tracks[y][x]
+
+    # Directional changes - (DirIn, Type): DirOut
+    fw_tracks = {
+        # Curves
+        ('n', 4608):  'w',
+        ('e', 4608):  's',
+        ('n', 16386): 'e',
+        ('w', 16386): 's',
+        ('s', 72):    'e',
+        ('w', 72):    'n',
+        ('s', 2064):  'w',
+        ('e', 2064):  'n',
+        # Switches
+        ('e', 37408): 's',
+        ('n', 17411): 'e',
+        ('w', 32872): 'n',
+        ('s', 3089):  'w',
+        ('w', 49186): 's',
+        ('s', 1097):  'e',
+        ('e', 34864): 'n',
+        ('n', 5633):  'w',
+        # Splits
+        ('e', 20994): 's',
+        ('w', 20994): 's',
+        ('n', 16458): 'e',
+        ('s', 16458): 'e',
+        ('e', 2136):  'n',
+        ('w', 2136):  'n',
+        ('n', 6672):  'w',
+        ('s', 6672):  'w'
+    }
+
+    to_left_tracks = {
+        # Switches
+        ('n', 37408): 'w',
+        ('w', 17411): 's',
+        ('s', 32872): 'e',
+        ('e', 3089): 'n',
+        # Crossings
+        ('n', 38433): 'w',
+        ('w', 50211): 's',
+        ('s', 33897): 'e',
+        ('e', 35889): 'n',
+        ('n', 38505): 'w',
+        ('s', 38505): 'e',
+        ('e', 52275): 'n',
+        ('w', 52275): 's',
+        # Splits
+        ('n', 20994): 'w',
+        ('w', 16458): 's',
+        ('s', 2136): 'e',
+        ('e', 6672): 'n'
+    }
+
+    to_right_tracks = {
+        # Switches
+        ('n', 49186): 'e',
+        ('w', 1097): 'n',
+        ('s', 34864): 'w',
+        ('e', 5633): 's',
+        # Crossings
+        ('e', 38433): 's',
+        ('n', 50211): 'e',
+        ('w', 33897): 'n',
+        ('s', 35889): 'w',
+        ('e', 38505): 's',
+        ('w', 38505): 'n',
+        ('n', 52275): 'e',
+        ('s', 52275): 'w',
+        # Splits
+        ('n', 20994): 'e',
+        ('w', 16458): 'n',
+        ('s', 2136): 'w',
+        ('e', 6672): 's'
+    }
+    # Check for the action type, if current track allows direction change
     if action == "move_forward":
-        track = tracks[y][x]
-        if track in dir_tr:
-            # Change direction: Curve or Switch-Merge
-            if dir == 'n':
-                if track == 4608: dir = 'w'
-                elif track == 16386: dir = 'e'
-                elif track == 16458: dir = 'e'
-                elif track == 6672: dir = 'w'
-                elif track == 17411: dir = 'e'
-                elif track == 5633: dir = 'w'
-            elif dir == 'e':
-                if track == 4608: dir = 's'
-                elif track == 2064: dir = 'n'
-                elif track == 20994: dir = 's'
-                elif track == 2136: dir = 'n'
-                elif track == 37408: dir = 's'
-                elif track == 34864: dir = 'n'
-            elif dir == 's':
-                if track == 72: dir = 'e'
-                elif track == 2064: dir = 'w'
-                elif track == 16458: dir = 'e'
-                elif track == 6672: dir = 'w'
-                elif track == 3089: dir = 'w'
-                elif track == 1097: dir = 'e'
-            elif dir == 'w':
-                if track == 16386: dir = 's'
-                elif track == 72: dir = 'n'
-                elif track == 20994: dir = 's'
-                elif track == 2136: dir = 'n'
-                elif track == 32872: dir = 'n'
-                elif track == 49186: dir = 's'
+        if (dir, track) in fw_tracks:
+            return fw_tracks[(dir, track)]
     elif action == "move_left":
-        if dir == 'n': dir = 'w'
-        elif dir == 'e': dir = 'n'
-        elif dir == 's': dir = 'e'
-        elif dir == 'w': dir = 's'
+        if (dir, track) in to_left_tracks:
+            return to_left_tracks[(dir, track)]
+        if invalid_path == None:
+            invalid_path = id
     elif action == "move_right":
-        if dir == 'n': dir = 'e'
-        elif dir == 'e': dir = 's'
-        elif dir == 's': dir = 'w'
-        elif dir == 'w': dir = 'n'
+        if (dir, track) in to_right_tracks:
+            return to_right_tracks[(dir, track)]
+        if invalid_path == None:
+            invalid_path = id
+    # If track does not allow the action: No dir change: path will be adjusted later
     return dir
 
 
